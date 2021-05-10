2acd0cd6a9c7de91f629129cfed44eb715fe73f2
Mano Fabian <mano.b.fabian@gmail.com>
2021-05-10 23:00:12 +0200

    done v 1.0

--- boosts.js
+++ boosts.js
@@ -1,14 +1,149 @@
 var boostsOnMap = [];
-var nextBoost = Math.random()*500;
+var nextBoost = 50;
+//Math.random()*500;
 var boostCounter = 0;
+var commonBoosts = [];
 
 const boosts = [
+    //accelerator 0-1
     {
-        color : "green",
+        effect : 2,
+        code : 1,
+        x : 0,
+        y : 0,
+        timeLeft : 1000,
+        src : "icons/acceleratorG.png",
+    },
+    {
+        effect : 3,
         code : 1,
         x : 0,
         y : 0,
         timeLeft : 500,
-        src : "accelerator.png",
-    }
+        src : "icons/acceleratorR.png",
+    },
+    //decelerator 2-3
+    {
+        effect : 2,
+        code : 2,
+        x : 0,
+        y : 0,
+        timeLeft : 1000,
+        src : "icons/deceleratorG.png",
+    },
+    {
+        effect : 3,
+        code : 2,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/deceleratorR.png",
+    },
+    //clear 4
+    {
+        effect : 1,
+        code : 3,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/clearB.png",
+    },
+    //ghost 5
+    {
+        effect : 2,
+        code : 4,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/ghostG.png",
+    },
+    //big 6
+    {
+        effect : 3,
+        code : 5,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/bigR.png",
+    },
+    //small 7
+    {
+        effect : 2,
+        code : 6,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/smallG.png",
+    },
+    //more 8
+    {
+        effect : 1,
+        code : 7,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/moreB.png",
+    },
+    //suck 9
+    {
+        effect : 3,
+        code : 8,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/suckR.png",
+    },
+    //thiefsDownfall 10
+    {
+        effect : 1,
+        code : 9,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/thiefsDownfallB.png",
+    },
+    //wallBreak 11
+    {
+        effect : 2,
+        code : 10,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/wallBreakG.png",
+    },
+    //noBorder 12
+    {
+        effect : 1,
+        code : 11,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/wallBreakB.png",
+    },
+    //square 13-14
+    {
+        effect : 2,
+        code : 12,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/squareG.png",
+    },
+    {
+        effect : 3,
+        code : 12,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/squareR.png",
+    },
+    //random 15
+    {
+        effect : 4,
+        code : 13,
+        x : 0,
+        y : 0,
+        timeLeft : 500,
+        src : "icons/random.png",
+    },
 ]
--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-
 var stoped = false;
 var everybodyin = false;
 var numberOfPLayers = 0;
 var startRound = true;
 var preloaded = true;
+var more = 0;
+var brightens = true;
+var border = 0;
 
 var gameMech = function() {
+    if(border > 0) {
+        border--;
+        document.getElementById("flexCanvas").style.border = "1px solid pink";
+    } else {
+        document.getElementById("flexCanvas").style.border = "1px solid yellow";
+    }
     var flex = document.getElementById("flexCanvas").getContext("2d");
     flex.clearRect(0, 0, 1000, 1000);
+    more = more == 0 ? 0 : more - 1;
     if(boostCounter > nextBoost) {
-        nextBoost = Math.random()*500;
+        nextBoost = Math.random() * (more == 0 ? 750 : 50);
         boostCounter = 0;
-        //console.log(Math.floor(Math.random()*10)+1);
-        var boostsCopy = boosts.slice();
-        var newBoost = boostsCopy[0];
+        var i = Math.floor(Math.random()*16);
+        var newBoost = {};
+        for(prop in boosts[i]) {
+            newBoost[prop] = boosts[i][prop];
+        }
         newBoost.x = Math.random()*960 + 20;
         newBoost.y = Math.random()*960 + 20;
         boostsOnMap.push(newBoost);
@@ -23,7 +34,12 @@ var gameMech = function() {
     boostsOnMap.forEach((boost) => {
         var img = new Image();
         img.src = boost.src;
-        flex.drawImage(img, boost.x, boost.y, 40, 40);
+        flex.drawImage(img, boost.x - 20, boost.y - 20, 40, 40);
+        flex.beginPath();
+        flex.lineWidth = 1;
+        flex.strokeStyle = "yellow";
+        flex.arc(boost.x, boost.y, 20, 0, 2*Math.PI);
+        flex.stroke();
     });
     players.forEach(mooving);
 }
@@ -33,16 +49,55 @@ clearInterval(int);
 
 function mooving(player) {
     if(player.chosed) {
+        var ghost = false;
+        var suck = false;
+        var wallBreak = false;
+        var square = false;
+        player.boosts.forEach((b) => {
+            ghost = b.code == 4;
+            suck = b.code == 8;
+            wallBreak = b.code == 10;
+            square = b.code == 12;
+        });
         if(player.alive) {
             var px = player.x;
             var py = player.y;
             if(player.right || player.left) {
-                player.l = player.right ? player.l + 0.017 : player.l-0.017;
+                if(!square) {
+                    if(suck) {
+                        player.l = player.right ? player.l - 0.016 : player.l + 0.016;
+                    } else {
+                        player.l = player.right ? player.l + 0.016 : player.l - 0.016;
+                    }
+                } else if(player.square) {
+                    if(suck) {
+                        player.l = player.right ? player.l - 0.5 : player.l + 0.5;
+                    } else {
+                        player.l = player.right ? player.l + 0.5 : player.l - 0.5;
+                    }
+                    player.square = false;
+                }
             }
-            player.x = player.x+Math.cos(player.l*Math.PI);
-            player.y = player.y+Math.sin(player.l*Math.PI);
+            player.x = player.x+Math.cos(player.l*Math.PI)*player.speed;
+            player.y = player.y+Math.sin(player.l*Math.PI)*player.speed;
+            if(wallBreak || border > 0) {
+                if(player.x < 0 - player.width) {
+                    px = 1000;
+                    player.x = 1000;
+                } else if(player.x > 1000 + player.width) {
+                    px = 0;
+                    player.x = 0;
+                }
+                if(player.y < 0 - player.width) {
+                    py = 1000;
+                    player.y = 1000;
+                } else if(player.y > 1000 + player.width) {
+                    py = 0;
+                    player.y = 0;
+                }
+            }
 
-            if(!player.inBreak) {
+            if(!player.inBreak && !ghost) {
                 var fix = document.getElementById("fixCanvas").getContext("2d");
                 fix.beginPath();
                 fix.lineWidth = player.width*2 + 1;
@@ -50,16 +105,16 @@ function mooving(player) {
                 fix.moveTo(px, py);
                 fix.lineTo(player.x, player.y);
                 fix.stroke();
-
-                var data = fix.getImageData(player.x+(Math.cos(player.l*Math.PI)*(player.width*1.5)),player.y+(Math.sin(player.l*Math.PI)*(player.width*1.5)), 1, 1);
+                
+                var data = fix.getImageData(player.x+(Math.cos(player.l*Math.PI)*(player.width+2)),player.y+(Math.sin(player.l*Math.PI)*(player.width+2)), 1, 1);
                 if(data.data[0] != 0 || data.data[1] != 0 || data.data[2] != 0) {
                     dropOut(player);
-                } else {
-                    data = fix.getImageData(player.x+(Math.cos((player.l+0.5)*Math.PI)*(player.width*1.9)),player.y+(Math.sin((player.l+0.5)*Math.PI)*(player.width*1.9)), 1, 1);
+                } else if(!square) {
+                    data = fix.getImageData(player.x+(Math.cos((player.l+0.5)*Math.PI)*(player.width+2)),player.y+(Math.sin((player.l+0.5)*Math.PI)*(player.width+2)), 1, 1);
                     if(data.data[0] != 0 || data.data[1] != 0 || data.data[2] != 0) {
                         dropOut(player);
                     } else {
-                        data = fix.getImageData(player.x+(Math.cos((player.l-0.5)*Math.PI)*(player.width*1.9)),player.y+(Math.sin((player.l-0.5)*Math.PI)*(player.width*1.9)), 1, 1);
+                        data = fix.getImageData(player.x+(Math.cos((player.l-0.5)*Math.PI)*(player.width+2)),player.y+(Math.sin((player.l-0.5)*Math.PI)*(player.width+2)), 1, 1);
                         if(data.data[0] != 0 || data.data[1] != 0 || data.data[2] != 0) {
                             dropOut(player);
                         }
@@ -72,7 +127,7 @@ function mooving(player) {
                     player.breakCount = 0;
                 }
             } else {
-                player.breakCount++;
+                player.breakCount = player.breakCount + player.speed;
                 if(player.breakCount >= 20) {
                     player.inBreak = false;
                     player.breakCount = 0;
@@ -81,20 +136,144 @@ function mooving(player) {
             
             }
 
-            if((player.x < 0 + player.width || player.x > 1000 - player.width || player.y < 0 + player.width || player.y > 1000- player.width) && player.alive) {
+            if((player.x < 0 + player.width || player.x > 1000 - player.width || player.y < 0 + player.width || player.y > 1000- player.width) && player.alive && !wallBreak && border == 0) {
                 dropOut(player);
             }
         }
 
         var flex = document.getElementById("flexCanvas").getContext("2d");
         flex.beginPath();
-        flex.arc(player.x,player.y,player.width,0,2*Math.PI);
-        flex.strokeStyle = "yellow";
-        flex.lineWidth = 0;
-        flex.fillStyle = "yellow";
+        if(!square) {
+            flex.arc(player.x,player.y,player.width,0,2*Math.PI);
+            flex.lineWidth = 1;
+        } else {
+            flex.lineWidth = player.width*2 + 1;
+            flex.moveTo(player.x-Math.cos(player.l*Math.PI)*(player.width+0.5), player.y-Math.sin(player.l*Math.PI)*(player.width+0.5));
+            flex.lineTo(player.x+Math.cos(player.l*Math.PI)*(player.width+0.5), player.y+Math.sin(player.l*Math.PI)*(player.width+0.5));
+        }
+        if(wallBreak) {
+            if(suck) {
+                flex.fillStyle  = "brown";
+            } else {
+                flex.fillStyle  = "pink";
+            }
+        } else {
+            flex.fillStyle = suck ? "blue" : "yellow";
+        }
         flex.fill();
         flex.stroke();
+
+        var i = -1;
+
+        boostsOnMap.forEach((boost, index) => {
+            if((player.x - boost.x)*(player.x - boost.x) + (player.y - boost.y)*(player.y - boost.y) < 500) {
+                activateBoost(player, boost);
+                i = index;
+            }
+        });
+        if(i > -1) {
+            boostsOnMap.splice(i, 1);
+            i = -1;
+        }
+        player.boosts.forEach((boost, index) => {
+            boost.timeLeft--;
+            if(boost.timeLeft == 0) {
+                deactivateBoost(player, index, boost);
+                i = index;
+            }
+        });
     }
+}
+
+function activateBoost(player, boost) {
+    if(boost.code == 1) {
+        if(boost.effect == 2) {
+            player.boosts.push(boost);
+            player.speed = player.speed*2;
+        } else {
+            players.forEach((p) => {
+                if(p.color != player.color) {
+                    p.boosts.push(boost);
+                    p.speed = p.speed*2;
+                }
+            })
+        }
+    } else if(boost.code == 2) {
+        if(boost.effect == 2) {
+            player.boosts.push(boost);
+            player.speed = player.speed/2;
+        } else {
+            players.forEach((p) => {
+                if(p.color != player.color) {
+                    p.boosts.push(boost);
+                    p.speed = p.speed/2;
+                }
+            })
+        }
+    } else if(boost.code == 3) {
+        var fix = document.getElementById("fixCanvas").getContext("2d");
+        fix.clearRect(0, 0, 1000, 1000);
+    } else if(boost.code == 4) {
+        player.boosts.push(boost);
+    } else if(boost.code == 5) {
+        players.forEach((p) => {
+            if(p.color != player.color) {
+                p.boosts.push(boost);
+                p.width = p.width*2;
+            }
+        })
+    } else if(boost.code == 6) {
+        player.boosts.push(boost);
+        player.width = player.width/2;
+    } else if(boost.code == 7) {
+        more = 500;
+    } else if(boost.code == 8) {
+        players.forEach((p) => {
+            if(p.color != player.color) {
+                p.boosts.push(boost);
+            }
+        })
+    } else if(boost.code == 9) {
+        players.forEach((p) => {
+           p.boosts = [];
+        })
+    } else if(boost.code == 10) {
+        player.boosts.push(boost);
+    } else if(boost.code == 11) {
+        border = 500;
+    } else if(boost.code == 12) {
+        if(boost.effect == 2) {
+            player.boosts.push(boost);
+        } else {
+            players.forEach((p) => {
+                if(p.color != player.color) {
+                    p.boosts.push(boost);
+                }
+            })
+        }
+    } else if(boost.code == 13) {
+        var i = Math.floor(Math.random()*15);
+        var newBoost = {};
+        for(prop in boosts[i]) {
+            newBoost[prop] = boosts[i][prop];
+        }
+        newBoost.x = Math.random()*960 + 20;
+        newBoost.y = Math.random()*960 + 20;
+        activateBoost(player, newBoost);
+    }
+}
+
+function deactivateBoost(player, index, boost) {
+    if(boost.code == 1) {
+        player.speed = player.speed/2;
+    } else if(boost.code == 2) {
+        player.speed = player.speed*2;
+    } else if(boost.code == 5) {
+        player.width = player.width/2;
+    } else if(boost.code == 6) {
+        player.width = player.width*2;
+    }
+    player.boosts.splice(index, 1);
 }
 
 function dropOut(player) {
@@ -111,22 +290,26 @@ function dropOut(player) {
         clearInterval(int);
         startRound = true;
         preloaded = false;
+        boostsOnMap = [];
         var bestPlayer = players[0];
         var secondPlayer;
         var i = 0;
-        for(var j = 0; j < players.length; j++) {
-            if(players[j].point > bestPlayer.point) {
-                bestPlayer = players[j];
-                i = j;
+        players.forEach((p, index) => {
+            p.boosts = [];
+            p.speed = 1;
+            p.width = 2;
+            if(p.point > bestPlayer.point) {
+                bestPlayer = p;
+                i = index;
             } else {
-                secondPlayer = players[j];
+                secondPlayer = p;
             }
-        }
-        for(var j = 0; j < players.length; j++) {
-            if(j != i && players[j].point > secondPlayer.point) {
-                secondPlayer = players[j];
+        });
+        players.forEach((p, index) => {
+            if(index != i && p.point > secondPlayer.point) {
+                secondPlayer = p;
             }
-        }
+        });
         if(bestPlayer.point >= (numberOfPLayers-1)*10 && bestPlayer.point > (secondPlayer.point+1)) {
             let konecHry = document.getElementById("konecHry");
             konecHry.style.visibility = "visible";
@@ -162,20 +345,27 @@ function dropOut(player) {
 }
 
 function keydown(e) {
-    for(var i = 0; i < players.length; i++) {
-        if(e.key == players[i].rKey) {
-            players[i].right = true;
+    players.forEach((p) => {
+        if(e.key == p.rKey) {
+            if(!p.right) {
+                p.square = true;
+            }
+            p.right = true;
         }
-        if(e.key == players[i].lKey) {
-            players[i].left = true;
+        if(e.key == p.lKey) {
+            if(!p.left) {
+                p.square = true;
+            }
+            p.left = true;
         }
-    }
+    })
     if(e.key == " ") {
         var fix = document.getElementById("fixCanvas");
         fix.style.visibility = "visible";
         fix = fix.getContext("2d");
         var flex = document.getElementById("flexCanvas");
         flex.style.visibility = "visible";
+        flex.style.border = "1px solid yellow";
         flex = flex.getContext("2d");
         if(everybodyin) {
             if(!preloaded && startRound) {
@@ -187,7 +377,6 @@ function keydown(e) {
                         player.y = Math.random()*960 + 20;
                         flex.beginPath();
                         flex.arc(player.x,player.y,player.width,0,2*Math.PI);
-                        flex.strokeStyle = "yellow";
                         flex.lineWidth = 0;
                         flex.fillStyle = "yellow";
                         flex.fill();
@@ -225,7 +414,6 @@ function keydown(e) {
                 if(player.chosed) {
                     flex.beginPath();
                     flex.arc(player.x,player.y,player.width,0,2*Math.PI);
-                    flex.strokeStyle = "yellow";
                     flex.lineWidth = 0;
                     flex.fillStyle = "yellow";
                     flex.fill();
@@ -245,14 +433,16 @@ function keydown(e) {
 }
 
 function keyup(e) {
-    for(var i = 0; i < players.length; i++) {
-        if(e.key == players[i].rKey) {
-            players[i].right = false;
+    players.forEach((p) => {
+        if(e.key == p.rKey) {
+            p.square = false;
+            p.right = false;
         }
-        if(e.key == players[i].lKey) {
-            players[i].left = false;
+        if(e.key == p.lKey) {
+            p.square = false;
+            p.left = false;
         }
-    }
+    });
 }
 
 function choseColor(i, keybox) {
